396d7b014bb119101babe11c52709f800ff22e42
PawlikJakub <41922381+Rapturr@users.noreply.github.com>
2021-06-09 19:51:08 +0200

    comments

--- desktop/src/com/niebo/project/desktop/DesktopLauncher.java
+++ desktop/src/com/niebo/project/desktop/DesktopLauncher.java
@@ -4,10 +4,26 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.niebo.project.MyHeaven;
 
+/**
+ * Główna klasa projektu.
+ */
 public class DesktopLauncher {
+	/**
+	 * Klasa main programu.
+	 */
 	public static void main (String[] arg) {
+		/**
+		 * @param config objekt, w którym przechowywana jest konfiguracja okna aplikacji.
+		 * @param forceExit jeśli jest false, to program może czasem działać w tle.
+		 * @param width ustawia szerokość okna
+		 * @param height ustawia wysokość okna
+		 * @param fullscreen mówi, czy można włączyć tryb pełnoekranowy
+		 * @param title ustawia tytuł okna
+		 * @param resizable mówi, czy rozmiar okna może być zmieniany
+		 */
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.forceExit = false;
+
+		config.forceExit = true;
 
 		config.width = 1280;
 		config.height = 720;
@@ -15,6 +31,9 @@ public class DesktopLauncher {
 		config.title = "Odysehja";
 		config.resizable = true;
 
+		/**
+		 * Wybiera klasę okna i przekazuje konfiguracje.
+		 */
 		new LwjglApplication(new MyHeaven(), config);
 	}
 }
